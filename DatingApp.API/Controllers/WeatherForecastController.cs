146383de2b5c542f975f1629fcedffe0c2618e04
rgt219 <rgt219@outlook.com>
2019-11-27 12:22:28 -0500

    First Commit

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public WeatherForecastController(DataContext context)
+        {
+            _context = context;
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+/*
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+*/
+        [HttpGet]
+        public async Task<IActionResult> GetForecastsAsync()
+        {
+            var forecasts = await _context.WeatherForecasts.ToListAsync();
+            return Ok(forecasts);
+
+            /*
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                WeatherForecastID = index,
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+            */
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetForecast(int id)
+        {
+            var forecast = await _context.WeatherForecasts.FirstOrDefaultAsync(x => x.WeatherForecastID == id);
+            return Ok(forecast);
+        }
+    }
+}
